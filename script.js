a516ce689bc48a8580f832dd3091c564ad120c8c
Gabrielmarcondess <149117554+Gabrielmarcondess@users.noreply.github.com>
2025-05-29 22:09:45 -0300

    aula fuunção

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("ola mundo")
+function validarForm(){
+    const inputNome =document.querySelector("#txt_nome");
+    const inputTurma =document.querySelector("#turma");
+    if (inputNome.value == ""){
+        exibirErr("erro");
+        return false;
+    };
+     if (inputTurma.value == ""){
+        exibirErr("erro");
+        return false;
+    };
+    return true;
+};
+function salvar(){
+    if( validarForm() == false){
+        return exibirErr("erro");
+    }
+    const nome = document.querySelector("#txt_nome");
+    const idade = document.querySelector("#idade");
+    exibirMsg("salvo com sucesso")
+   
+};
+function exibirMsg(msg){
+    const divmsg = document.querySelector("#msg");
+    divmsg.innerHTML = msg;
+};
+function exibirErr(msg){
+    const divmsg = document.querySelector("#msg");
+    divmsg.innerHTML = msg;
+atividade();
+}
